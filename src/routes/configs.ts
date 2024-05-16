db5735a757958e70e7b4b6d3d9b722d36ffd7065
zernovdm <dmitriy.only.work@mail.ru>
2024-05-16 20:33:46 +0300

    первый модуль закончен, добавлен пдф генератор как делали на паре

--- src/routes/configs.ts
+++ src/routes/configs.ts
@@ -1,4 +1,5 @@
 export const HOME_ROUTE = 'home';
 export const CONTACTS_ROUTE = 'contacts';
 export const ABOUT_ROUTE = 'about';
+export const DOWNLOAD_ROUTE = 'download'
 export const ORDER_ROUTE = 'order';
